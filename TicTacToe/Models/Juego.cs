6f74a2e4e8f8921bd3bbe785725ef25c004a2a7f
larissa-jpg-0 <larissa.cristerna@cetys.edu.mx>
2026-05-12 10:13:48 -0700

    Subir jugador y juego

---
+++ TicTacToe/Models/Juego.cs
@@ -0,0 +1,30 @@
+namespace TicTacToe.Models;
+public class Juego
+{
+public Jugador JugadorActual { get; set; }
+public Jugador Jugador1 { get; set; }
+public Jugador Jugador2 { get; set; }
+public Tablero Tablero { get; set; }
+public Juego()
+{
+Jugador1 = new Jugador("Jugador 1", "X");
+Jugador2 = new Jugador("Jugador 2", "O");
+JugadorActual = Jugador1;
+Tablero = new Tablero();
+}
+public bool Jugar(int posicion)
+{
+return Tablero.ColocarSimbolo(posicion, JugadorActual.Simbolo);
+}
+public bool HayGanador()
+{
+return Tablero.VerificarGanador(JugadorActual.Simbolo);
+}
+public void CambiarTurno()
+{
+if (JugadorActual == Jugador1)
+JugadorActual = Jugador2;
+else
+JugadorActual = Jugador1;
+}
+}
